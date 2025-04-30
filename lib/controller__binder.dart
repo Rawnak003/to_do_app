80af84e8cf38530fec98db71986defa869ee237b
Rawnak003 <shmrawnak@gmail.com>
2025-04-30 16:33:48 +0600

    Add #17: User details and password update with Getx Implement

--- lib/controller__binder.dart
+++ lib/controller__binder.dart
@@ -5,6 +5,7 @@ import 'package:to_do_application/presentation/controllers/completed_task_list_c
 import 'package:to_do_application/presentation/controllers/fab_visibility_controller.dart';
 import 'package:to_do_application/presentation/controllers/forget_password_controller.dart';
 import 'package:to_do_application/presentation/controllers/forget_password_pin_verify_controller.dart';
+import 'package:to_do_application/presentation/controllers/image_picker_controller.dart';
 import 'package:to_do_application/presentation/controllers/login_controller.dart';
 import 'package:to_do_application/presentation/controllers/main_bottom_nav_controller.dart';
 import 'package:to_do_application/presentation/controllers/new_task_list_controller.dart';
@@ -13,6 +14,8 @@ import 'package:to_do_application/presentation/controllers/register_controller.d
 import 'package:to_do_application/presentation/controllers/reset_password_controller.dart';
 import 'package:to_do_application/presentation/controllers/show_password_controller.dart';
 import 'package:to_do_application/presentation/controllers/task_status_count_list_controller.dart';
+import 'package:to_do_application/presentation/controllers/user_password_update_controller.dart';
+import 'package:to_do_application/presentation/controllers/user_profile_update_controller.dart';
 
 class ControllerBinder extends Bindings{
   @override
@@ -31,5 +34,8 @@ class ControllerBinder extends Bindings{
     Get.put(ProgressTaskListController());
     Get.put(CompletedTaskListController());
     Get.put(CancelledTaskListController());
+    Get.put(ImagePickerController());
+    Get.put(UserProfileUpdateController());
+    Get.put(UserPasswordUpdateController());
   }
 }
